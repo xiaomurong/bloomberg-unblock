8df3f369d7161b94318604b9c3ee80149856b412
samchu11 <faisam2006@protonmail.com>
2020-07-16 23:07:01 +0800

    1.auto format 2.able to block with prue JS but #graphics-paywall-overlay still show after load html content

--- content.js
+++ content.js
@@ -1,34 +1,8 @@
 console.log("🅱️ SCRIPT STARTED 🅱️");
 try {
-	(function overwrite(link) {
-			var xhr = new XMLHttpRequest();
-			xhr.open('GET', link.href);
-			xhr.onload = () => {
-				var html = xhr.responseText;
-				// HK, US
-				html = html.replace('class="hub-main" style="display: none;"');
-				html += "<style>#graphics-paywall-overlay{position: static!important;}</style>";
-				html += "<style>html {overflow:initial!important;}";
-				
-				
-				
-document.addEventListener('DOMSubtreeModified', function(e) {
-    if(document.getElementsByClassName("hub-main").length > 0){
-        document.getElementsByClassName("hub-main")
-		document.getElementsByClassName("hub-main").style.display = "none";
-    }
-});
-  
-				
-//document.body.innerHTML = html;
-
-			};
-			xhr.send();
-			
-			
-		//}
-	})(location);
+  document.getElementById("graphics-paywall-overlay").style.display = "none";
+  document.getElementsByTagName("html")[0].style.overflow = "scroll";
 } catch (ex) {
-	console.log("Error: " + ex.message);
+  console.log("Error: " + ex.message);
 }
 console.log("🅱️ SCRIPT ENDED 🅱️");
